e755736ce05efc9a7fc517692fff1f6409c97212
Charles Shih <schrht@gmail.com>
2021-01-12 11:21:14 +0800

    Bugfix and more information covered.

--- avocado_cloud/summarize_results.sh
+++ avocado_cloud/summarize_results.sh
@@ -2,28 +2,35 @@
 
 # Description:
 #   This script summarize the test results for avocodo-cloud.
-#
+# Limiation:
+#   Only 1 flavor can be tested each time the avocodo-cloud runs.
 
 function collect_results() {
 	fdir=$1
 	fres=$fdir/results.json
 	flog=$fdir/job.log
 
+	res_t=$(cat $fres | jq -r '.total')
 	res_p=$(cat $fres | jq -r '.pass')
 	res_c=$(cat $fres | jq -r '.cancel')
-	res_e=$(cat $fres | jq -r '.error')
+	res_e=$(cat $fres | jq -r '.errors')
 	res_f=$(cat $fres | jq -r '.failures')
 	res_s=$(cat $fres | jq -r '.skip')
 	logid=$(cat $fres | jq -r '.debuglog' | sed 's#.*\(job-20.*\)/job.log#\1#')
 
-        table="${table}$(printf '%s,%s,%s,%s,%s,%s' $logid $res_p $res_f $res_e $res_c $res_s)\n"
+	flavor=$(grep -m 1 'key=name.*path=.*Flavor.*=>' $flog | cut -d "'" -f 2)
+	azone=$(grep -m 1 'key=az.*path=.*VM.*=>' $flog | cut -d "'" -f 2)
+	imgname=$(grep -m 1 'key=name.*path=.*Image.*=>' $flog | cut -d "'" -f 2)
+	imgid=$(grep -m 1 'key=id.*path=.*Image.*=>' $flog | cut -d "'" -f 2)
+
+	table="${table}$(printf '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s' $logid $flavor $azone $imgname $imgid $res_t $res_p $res_f $res_e $res_c $res_s)\n"
 }
 
 # Parse parameters
 if [ -z $1 ]; then
 	echo "Usage: $(basename $0) <dirs>" >&2
 	echo "Notes:" >&2
- 	echo "- dirs: avocado-cloud log dirs starts with 'job-'." >&2
+	echo "  - dirs: avocado-cloud log dirs starts with 'job-'." >&2
 	exit 1
 fi
 
@@ -35,7 +42,6 @@ for d in $dlist; do
 done
 
 # Show the summary as a table
-echo -e $table | column -t -s ',' -R 2,3,4,5,6 -N LogID,PASS,FAIL,ERROR,CANCEL,SKIP
+echo -e $table | column -t -s ',' -R 6,7,8,9,10,11 -N LogID,Flavor,AZone,ImageName,ImageID,TOTAL,PASS,FAIL,ERROR,CANCEL,SKIP
 
 exit 0
-
