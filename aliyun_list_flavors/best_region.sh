992d1a8c8ac8b78aa3e850d9ace7c113bc3e08c8
Charles Shih <schrht@gmail.com>
2021-07-06 18:31:54 +0800

    Update logic for best_region, support zone and flavor lookup.

--- aliyun_list_flavors/best_region.sh
+++ aliyun_list_flavors/best_region.sh
@@ -6,6 +6,9 @@
 
 PATH=$PATH:.
 
+[ "$1" = "-f" ] && flavor=$2
+[ "$1" = "-z" ] && zone=$2
+
 [ -f ./pass.txt ] && grep -v -f ./pass.txt ./full.txt > /tmp/todo.txt || cat ./full.txt > /tmp/todo.txt
 
 resource_matrix=/tmp/aliyun_flavor_distribution.txt
@@ -13,16 +16,31 @@ if [ -f $resource_matrix ]; then
 	echo "Notice: '$resource_matrix' was updated at $(stat -c %z $resource_matrix)." >&2
 	echo "Notice: You might consider running 'query_flavors.sh' again to get the latest status." >&2
 else
-	query_flavors.sh || exit 1
+	query_flavors.sh >&2 || exit 1
 fi
 
-for flavor in $(cat /tmp/todo.txt); do
-	echo -e "------\n$flavor:"
-	grep $flavor $resource_matrix | cut -d, -f1
-done
+# get matrix
+grep -f /tmp/todo.txt $resource_matrix > /tmp/matrix.txt
+
+# show status
+if [ ! -z $flavor ]; then
+	# show flavor status
+	echo -e "======\nFLAVOR STATUS\n------\n$flavor:" >&2
+	grep $flavor /tmp/matrix.txt | cut -d, -f1
+fi
+
+
+# show zone status
+if [ ! -z $zone ]; then
+	echo -e "======\nZONE STATUS\n------\n$zone:" >&2
+	grep $zone /tmp/matrix.txt | cut -d, -f2
+fi
 
 # get best region
-echo "======"
-grep -f /tmp/todo.txt $resource_matrix | cut -d, -f1 | uniq -c | sort -nr
+if [ -z $flavor ] && [ -z $zone ]; then
+	echo -e "======\nBEST REGIONS\n------" >&2
+	cat /tmp/matrix.txt | cut -d, -f1 | uniq -c | sort -nr
+fi
 
 exit 0
+
